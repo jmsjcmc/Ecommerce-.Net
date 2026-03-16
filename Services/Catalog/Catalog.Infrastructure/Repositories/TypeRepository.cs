4ce8c0b4588b6cd17b01a2569655e14922f59a9a
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-03-16 10:20:20 +0800

    feat: add brand repo

---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/TypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class TypeRepository: ITypeRepository
+    {
+        private readonly IMongoCollection<Pr>
+        public TypeRepository(IConfiguration config)
+        {
+            
+        }
+
+        public Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductType> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
