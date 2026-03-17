71f0c849f6d415873458ef2ff405cf645130cfb8
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-03-17 12:47:15 +0800

    feat: add product query, handler, and mapper

---
+++ Services/Catalog/Catalog.Application/Queries/GetAllProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Application.Responses;
+using Catalog.Core.Specifications;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public record GetAllProductQuery(CatalogSpecParams catalogSpecParams): IRequest<Pagination<ProductResponse>>
+    {
+    }
+}
