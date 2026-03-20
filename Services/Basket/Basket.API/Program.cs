9c82622860fa52c634106fc8b4115c9672d963f2
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-03-20 10:53:47 +0800

    feat: update program.cs and appsetting

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,9 @@
+using Basket.Application.Handlers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+using Basket.Infrastructure.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +11,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+// Add application services
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+// Add swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Register mediatR
+var assemblies = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof(CreateShoppingCartHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+// Redis
+builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
+builder.Services.AddStackExchangeRedisCache(())
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
