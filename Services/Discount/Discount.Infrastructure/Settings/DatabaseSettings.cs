09895915ac4cbbffa31e413a3a1fbcdb8f07606e
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-04-02 09:36:10 +0800

    feat: update program.cs

---
+++ Services/Discount/Discount.Infrastructure/Settings/DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discount.Infrastructure.Settings
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
